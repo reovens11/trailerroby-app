329856587ff1c51a9549865061fe62181f0c2757
TrailerRoby <koralbabila@gmail.com>
2026-05-11 23:52:37 +0300

    Add location permissions and WebView geolocation handler

--- android/app/src/main/java/com/trailerroby/app/MainActivity.java
+++ android/app/src/main/java/com/trailerroby/app/MainActivity.java
@@ -1,5 +1,21 @@
 package com.trailerroby.app;
 
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        WebView webView = getBridge().getWebView();
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin,
+                    GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
+    }
+}
